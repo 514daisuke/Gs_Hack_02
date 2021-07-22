789e0ebd085149f978ce51b26c3e1e5b58c7c983
Youichi-Higa <yo1_1023@yahoo.co.jp>
2021-07-22 18:14:44 +0900

    firstcommit

---
+++ choice1.php
@@ -0,0 +1,36 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>選択肢１</h1>
+
+    <div>
+        <a href='choice2.php?choice01=1'>のんびりしたい</a>
+    </div>
+    <div>
+        <a href='choice2.php?choice01=2'>刺激が欲しい</a>
+    </div>
+    <div>
+        <a href='choice2.php?choice01=3'>アクティブに動きたい</a>
+    </div>
+
+
+    <!-- <button onclick="location.href='choice2.php?choice01=1'">のんびりしたい</button> -->
+    <!-- <a href='Negotiation_act.php?Target_id=<?= $Target_result["id"] ?>&My_id=<?= $My_result["id"] ?>'>交換依頼</a> -->
+
+
+
+</body>
+
+</html>
