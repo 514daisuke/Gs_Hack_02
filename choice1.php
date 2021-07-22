383e05b38c1de2460d6f831f9c202428d867b72f
Youichi-Higa <yo1_1023@yahoo.co.jp>
2021-07-23 02:48:40 +0900

    result.phpの編集

--- choice1.php
+++ choice1.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/choice.css">
     <title>Document</title>
 </head>
 
@@ -16,13 +17,13 @@
     <h1>選択肢１</h1>
 
     <div>
-        <a href='choice2.php?count=1'>のんびりしたい</a>
+        <a href='choice2.php?count=1' class="btn-sticky">のんびりしたい</a>
     </div>
     <div>
-        <a href='choice2.php?count=2'>刺激が欲しい</a>
+        <a href='choice2.php?count=2' class="btn-sticky">刺激が欲しい</a>
     </div>
     <div>
-        <a href='choice2.php?count=3'>アクティブに動きたい</a>
+        <a href='choice2.php?count=3' class="btn-sticky">アクティブに動きたい</a>
     </div>
 
 
